94f253d4cbc46f8e98e90b8bee87901c3a151eb4
Evan <eburan15@outlook.com>
2018-10-02 14:23:50 -0600

    Flags file now checks the type of the arguments and requires .midi/.mid and .pno files

--- flags.py
+++ flags.py
@@ -1,35 +1,41 @@
 
 from argparse import ArgumentParser
+import argparse
 import sys
+
+def pno_type(path):
+    if not path.endswith('.pno'):
+         raise argparse.ArgumentTypeError('A .pno file is required for input')
+    return path
+
+def midi_type(path):
+    if not path.endswith('.midi') and not path.endswith('.mid'):
+        raise argparse.ArgumentTypeError('A .midi or .mid file is required for the output name')
+    return path
 
 # Take in compilation arguments
 # Stuff like -o for output file name
 parser = ArgumentParser(description='Compile a .pno file to a .midi file')
 
 
-parser.add_argument("python",  nargs=1, help='A .py file is needed for input')
-parser.add_argument("midi",  nargs=1, help='A .midi or .mid file is needed for input')
-parser.add_argument("piano",  nargs=1, help='A .pno file is needed for input')
+#parser.add_argument("python",  nargs=1, help='A .py file is needed for input')
+parser.add_argument('midi_out', type=midi_type, help='A .midi or .mid file is needed for output')
+parser.add_argument('pno_in', type=pno_type, help='A .pno file is needed for input')
 
 
+args = parser.parse_args()
 
-args = parser.parse_args(sys.argv)
+#if len(sys.argv) != 3:
+#    print('usage: flags.py out.midi in.pno\n')
+
+midi_file = args.midi_out
+piano_file = args.pno_in
+
 
 
 print("------------------")
-print(args.python[0])
-print(args.midi[0])
-print(args.piano[0])
-#if args.piano[0] == "sample.pno":
-#    print("I DID IT")
-#else:
-#    print("Did not get correct name")
-if  args.piano[0].endswith('.pno'):
-    print("YAY PART 2") 
-else:
-    print("Incorrect file type.  Requires .pno file")
-
-
-print("*******************")
+print "Midi file output: ", midi_file
+print "Piano file input: ", piano_file
+print("------------------")
 
 
