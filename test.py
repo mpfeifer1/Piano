1f1160ecbd500a698ac3c5923f495e4e428fd98e
Michael Pfeifer <mikepf97@gmail.com>
2018-10-01 20:54:26 -0600

    Got rudimentary grammar working

--- test.py
+++ test.py
@@ -1,48 +1,48 @@
 from mido import Message, MidiFile, MidiTrack
-from lark import Lark
+
+import lark
 from argparse import ArgumentParser
+from lark import Lark
 import unittest
 import sys
 
-# Take in compilation arguments
-# Stuff like -o for output file name
-parser = ArgumentParser(description='Compile a .pno file to a .midi file')
-parser.add_argument('-o', nargs=1,  help='Choose a new file name')
+grammar = r'''
+    start: compose
+    digit: "0"
+        | "1"
+        | "2"
+        | "3"
+        | "4"
+        | "5"
+        | "6"
+        | "7"
+        | "8"
+        | "9"
+    accidental: "#" | "b"
+    compose: digit | accidental | comment
+    comment: "//"
 
-thing = parser.parse_args(sys.argv)
-print(thing)
-
-# Build Midi file and tracks
-mid = MidiFile()
-track1 = MidiTrack()
-track2 = MidiTrack()
-track3 = MidiTrack()
-track4 = MidiTrack()
-mid.tracks.append(track1)
-mid.tracks.append(track2)
-mid.tracks.append(track3)
-mid.tracks.append(track4)
-
-#track.append(Message('program_change', program=12, time=0))
-
-# Add notes to tracks
-track1.append(Message('note_on', note=64, velocity=64, time=0))
-track1.append(Message('note_off', note=64, velocity=64, time=1024))
-track2.append(Message('note_on', note=68, velocity=64, time=128))
-track2.append(Message('note_off', note=68, velocity=64, time=1024))
-track3.append(Message('note_on', note=71, velocity=64, time=256))
-track3.append(Message('note_off', note=71, velocity=64, time=1024))
-track4.append(Message('note_on', note=76, velocity=64, time=384))
-track4.append(Message('note_off', note=76, velocity=64, time=1024))
+    %import common.WS_INLINE
+    %ignore WS_INLINE
+'''
 
 # Try a sample grammar recognition
-l = Lark('''start: WORD "," WORD "!"
-            %import common.WORD
-            %ignore " "
-         ''')
-print(l.parse("Hello, World!").pretty())
+l = lark.Lark(grammar)
+strs = [
+    "5",
+    "INVALID%$&",
+    "5\t5",
+    "1 2",
+    "1 ",
+    " 1",
+    "\t6",
+    "// hello"
+]
 
-
-# Sample output
-print(mid.length)
-mid.save('song.midi')
+for i in strs:
+    print(i)
+    try:
+        print(l.parse(i).pretty())
+    except:
+        print("Didn't work")
+    print()
