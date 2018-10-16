fdb538f6cc9de37fe87680970e67c69402b9acba
Michael Pfeifer <mikepf97@gmail.com>
2018-10-15 19:44:40 -0600

    Integrated flags into main program, created a skeleton semantic analyzer

--- launch.py
+++ launch.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-from mido import Message, MidiFile, MidiTrack
-
-import lark
-from argparse import ArgumentParser
 from lark import Lark
+from mido import Message, MidiFile, MidiTrack
+from argparse import ArgumentParser
+
 import unittest
+import lark
 import sys
 
 import grammar
+import semantic
 import ourtests
+import flags
+
+inputflags = flags.getcommandlineargs()
+print(inputflags)
+
+with open(inputflags['piano_file'], 'r') as inputfile:
+    data = inputfile.read()
 
 l = lark.Lark(grammar.getgrammar(), parser='lalr', lexer="contextual")
+tree = l.parse(data);
+result = semantic.analyze(tree)
