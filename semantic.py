d017b256510018e058f4879d50d9d774c0fc7714
Michael Pfeifer <mikepf97@gmail.com>
2018-10-15 20:14:11 -0600

    Added more function stubs in the semantic analyzer

--- semantic.py
+++ semantic.py
@@ -1,3 +1,6 @@
+#in this file, we may want a global state object
+# That, or make this a class like normal people
+
 def analyze(tree):
     # so basically here, we're gonna wanna loop through
     # every command in the program, check if it's valid
@@ -8,8 +11,44 @@ def analyze(tree):
     # to each measure
     pass
 
+# check that all the numbers are powers of 2 and nonzero
 def is_valid_division(tree):
     pass
 
+# check the name exists in our program
 def is_valid_identifier(tree):
     pass
+
+# Check if it has a 'start', and one compose
+def is_valid_program(tree):
+    pass
+
+# Takes in a measure, builds a list of signals
+def measure_to_signal(tree, time):
+    pass
+
+def chord_to_signal(tree, time):
+    pass
+
+def tuple_to_signal(tree, time):
+    pass
+
+def note_to_signal(tree, time):
+    pass
+
+# given a tree that represents a dynamic, set the new volume
+def apply_dynamic(tree):
+    pass
+
+# returns a new tree without the base repeat
+def expand_repeat(tree):
+    pass
+
+# given a tree that represents a tempo, set that new tempo
+def apply_tempo(tree):
+    pass
+
+# given a tree that represents a time signature, set the timesig
+def apply_timesig(tree):
+    pass
+
