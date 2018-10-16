ff45676928839532bcfe08ba2cb116df4ff40105
Michael Pfeifer <mikepf97@gmail.com>
2018-10-15 19:57:04 -0600

    Put some thoughts for semantic analyzer down

--- semantic.py
+++ semantic.py
@@ -1,2 +1,15 @@
-def analyze():
+def analyze(tree):
+    # so basically here, we're gonna wanna loop through
+    # every command in the program, check if it's valid
+    # and do it.
+    # we gonna need to keep track of variables defined and stuff
+    # it should return nice errors, yeah
+    # this should keep track of global time, and send that in
+    # to each measure
+    pass
+
+def is_valid_division(tree):
+    pass
+
+def is_valid_identifier(tree):
     pass
