bcf36ac6dda00179ab34419af843fa6a5195bf71
NDucasse <nathanducasse@gmail.com>
2018-10-18 19:24:35 -0600

    Forgot to push changes to ourtests.py

--- ourtests.py
+++ ourtests.py
@@ -5,13 +5,16 @@ from testhelper import TestHelp
 import lark
 from lark import Tree
 from lark import lexer
-
+from lark import exceptions
 Token = lexer.Token
+LarkError = exceptions.LarkError
 
 
 class TestGrammar(unittest.TestCase):
+
     def setUp(self):
         self.l = lark.Lark(grammar.getgrammar(), parser='lalr', lexer="contextual")
+        self.help = TestHelp()
 
 
     def test_comment(self):
@@ -21,14 +24,12 @@ class TestGrammar(unittest.TestCase):
             // Nathan is a nerd
         }
         """
-        expected = Tree('start', [Tree('compose', [])])
-
-        ''' Act '''
-        actual = self.l.parse(test)
-        #print(actual)
-
-        ''' Assert '''
-        self.assertEqual(actual, expected, 'Comments not parsed correctly')
+        expected ='''
+        start
+            compose
+        '''
+        actual = self.l.parse(test).pretty()
+        self.assertTrue(self.help.prettyTreeComp(actual, expected), 'Comments not parsed correctly')
 
 
     def test_basicMeasure(self):
@@ -58,14 +59,56 @@ class TestGrammar(unittest.TestCase):
                         B
                         accidental b
                         number 4
+        '''
+        
+        testtree = self.l.parse(test).pretty()
+        self.assertTrue(self.help.prettyTreeComp(testtree, accept), 'Basic Measure syntax parsed incorrectly')
 
+
+    def test_repeat(self):
+        test='''
+        Compose {
+            Repeat
+            Tempo(72)
+            Endr
+        }
+        '''
+        accept = '''
+        start
+            compose
+                composeitems
+                    repeat
+                        composeitems
+                            tempo
+                                number 72
         '''
         testtree = self.l.parse(test).pretty()
+        self.assertTrue(self.help.prettyTreeComp(testtree, accept), 'Basic repeat tree incorrect')
         
-        x = TestHelp()
-        res = x.prettyTreeComp(testtree, accept)
         
-        self.assertTrue(res, 'Basic Measure syntax parsed incorrectly')
+    def test_repeatFail(self):
+        test='''
+        Compose {
+            Repeat
+            Tempo(72)
+        }
+        ''' 
+        reject = '''
+        start
+            compose
+                composeitems
+                    repeat
+                        composeitems
+                            tempo
+                                number 72
+        '''
+        # Lark throws an exception if the test string doesn't match the grammar.
+        try:
+            testtree = self.l.parse(test).pretty()
+            testfail = False
+        except LarkError as e:
+            testfail = True
+        self.assertTrue(testfail, 'Incorrect repeat structure accepted (no Endr)')
 
 '''
     """
