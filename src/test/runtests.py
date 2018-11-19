6558a589dcfbf2968e6334081f7dddd1b8dd796b
Michael Pfeifer <mikepf97@gmail.com>
2018-11-18 18:17:26 -0700

    Made Run Tests return proper error code

--- src/test/runtests.py
+++ src/test/runtests.py
@@ -1,4 +1,9 @@
 import os
+
 tests = {'semantictests.py', 'grammartests.py'}
+
+code = 0
 for test in tests:
-    os.system('python3 ' + test)
+    if os.system('python3 ' + test) == 1:
+        code = 1
+exit(code)
