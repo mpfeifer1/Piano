42fa8ba04ed70d0f324d8bdd406b46eb9d03162a
Michael Pfeifer <mikepf97@gmail.com>
2018-11-28 20:47:45 -0700

    Test file will check for failures again (#50)

--- src/test/runtests.py
+++ src/test/runtests.py
@@ -4,6 +4,6 @@ tests = {'semantictests.py', 'grammartests.py', 'miditests.py'}
 
 code = 0
 for test in tests:
-    if os.system('python3 ' + test) == 1:
+    if os.system('python3 ' + test) != 0:
         code = 1
 exit(code)
