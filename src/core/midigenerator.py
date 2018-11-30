31f8268c07ff8e28149001fac8bc8ba200cad763
Hannah Wegehaupt <hawegehaupt@gmail.com>
2018-11-29 17:53:28 -0700

    Added time signature support

--- src/core/midigenerator.py
+++ src/core/midigenerator.py
@@ -9,6 +9,7 @@ class MidiGenerator:
     def __init__(self, signals):
         # Save the signals the user passed in
         self.signals = signals
+        self.timesig = 1000
         self.tempo = 120
         self.dynamic = 87
         self.measure_start_time = 0
@@ -93,7 +94,7 @@ class MidiGenerator:
         #add an initial track to the song
         self.add_track()
 
-        self.song.tracks[self.current_track].append(self.midify_tempo({'type': 'tempo', 'bpm': 20}))
+        self.song.tracks[self.current_track].append(self.midify_tempo({'type': 'tempo', 'bpm': 60}))
 
         for signal in self.signals:
             print(signal)
@@ -108,6 +109,9 @@ class MidiGenerator:
                 self.song.tracks[self.current_track].append(off)
             elif signal['type'] == 'dynamic':
                 self.midify_dynamic(signal)
+            elif signal['type'] == 'timesig':
+                self.midify_timesig(signal)
+
 
         self.song.save('piano.mid')
 
@@ -121,6 +125,12 @@ class MidiGenerator:
         #self.current_track = 0
         #self.current_channel = 0
         #self.first_instrument = True
+
+    def midify_timesig(self, signal):
+        if signal['type'] != 'timesig':
+            print('Error: invalid timesig signal')
+
+        self.timesig = 1000 * signal['time_denom']
 
     def midify_dynamic(self, signal):
         if signal['type'] != 'dynamic':
@@ -170,15 +180,17 @@ class MidiGenerator:
             return '', ''
 
         noteNumber = noteToNumber[signal['note_name']]
-        length = int(1000 * (signal['length_num']/float(signal['length_denom'])))
+        length = int(self.timesig * (signal['length_num']/float(signal['length_denom'])))
 
         self.measure_end_time = self.measure_start_time + length
 
         #QQQXXX DEBUG
         self.file.write('song.tracks[' + str(self.current_track) + '].append(Message("note_on", note='
-            + str(noteNumber) + ', channel=' + str(self.current_channel) + ', velocity=' + str(self.dynamic) + ', time=0))\n')
+            + str(noteNumber) + ', channel=' + str(self.current_channel) +
+            ', velocity=' + str(self.dynamic) + ', time=0))\n')
         self.file.write('song.tracks[' + str(self.current_track) + '].append(Message("note_off", note='
-            + str(noteNumber) + ', channel=' + str(self.current_channel) + ', velocity=' + str(self.dynamic) + ', time=' +
+            + str(noteNumber) + ', channel=' + str(self.current_channel) +
+            ', velocity=' + str(self.dynamic) + ', time=' +
             str(length) + '))\n')
 
 
@@ -195,7 +207,9 @@ class MidiGenerator:
         self.max_track += 1
         self.current_track = self.max_track
         self.track_time.append(0)
-        self.song.tracks[self.max_track].append(Message("note_on", note=0, channel=self.current_channel, velocity=0, time=0))
-        self.song.tracks[self.max_track].append(Message("note_off", note=0, channel=self.current_channel, velocity=0, time=self.measure_start_time))
+        self.song.tracks[self.max_track].append(Message("note_on", note=0,
+            channel=self.current_channel, velocity=0, time=0))
+        self.song.tracks[self.max_track].append(Message("note_off", note=0,
+            channel=self.current_channel, velocity=0, time=self.measure_start_time))
 
 
