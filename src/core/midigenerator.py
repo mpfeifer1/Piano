07c6123a05dcd5cbc8313c4fa232ec7bf4931434
Michael Pfeifer <mikepf97@gmail.com>
2018-12-04 13:05:30 -0700

    Code works slightly more reliably

--- src/core/midigenerator.py
+++ src/core/midigenerator.py
@@ -14,11 +14,14 @@ class MidiGenerator:
         self.dynamic = 87
         self.measure_start_time = 0
         self.ticks_per_beat = 1000
+
         self.song = MidiFile(type=1, ticks_per_beat=1000)
-        self.max_track = -1
-        self.current_track = -1
         self.track_time = []
-        self.first_instrument = True
+        for i in range(32):
+            self.song.tracks.append(MidiTrack())
+            self.track_time.append(0)
+
+        self.current_track = 0
         self.current_channel = 0
 
     # Returns a dictionary from each signal type to a list
@@ -39,9 +42,9 @@ class MidiGenerator:
 
     # Check that the list of signals passed in is valid
     def validate(self, signals):
+        print('validate')
         # Grab valid types
         types = self.get_type_fields()
-
 
         # Check the signal has a type
         for signal in signals:
@@ -74,14 +77,12 @@ class MidiGenerator:
 
     # Take the list of signals, and build a midi file
     def generate(self):
+        print('generate')
         # If the signals passed in aren't valid, return an error
         if not self.validate(self.signals):
             return False
 
-        #add an initial track to the song
-        self.add_track()
-
-
+        self.resolve_errors(0)
         for signal in self.signals:
             if signal['type'] == 'measure':
                 self.midify_measure(signal)
@@ -99,8 +100,6 @@ class MidiGenerator:
                 self.song.tracks[self.current_track].append(off)
                 self.track_time[self.current_track] += off.time
             elif signal['type'] == 'chord':
-
-                self.resolve_errors(0)
                 initialTrack = self.current_track
                 for i in range(len(signal['notes'])):
                     note = signal['notes'][i]
@@ -109,14 +108,9 @@ class MidiGenerator:
                     self.song.tracks[self.current_track].append(on)
                     self.song.tracks[self.current_track].append(off)
                     self.track_time[self.current_track] += off.time
-                    if i == len(signal['notes'])-1:
-                        break
-                    if self.current_track == self.max_track:
-                        self.add_track()
-                    else:
-                        self.current_track += 1
+                    self.current_track += 1
+                    self.current_channel += 1
                 self.current_track = initialTrack
-                self.resolve_errors(0)
 
             elif signal['type'] == 'dynamic':
                 self.midify_dynamic(signal)
@@ -130,28 +124,31 @@ class MidiGenerator:
         return self.song
 
     def resolve_errors(self, time):
-        for i in range(len(self.song.tracks)):
+        print('resolve')
+        for i in range(len(self.track_time)):
             if time < self.track_time[i]:
                 time = self.track_time[i]
 
-        for i in range(len(self.song.tracks)):
+        for i in range(len(self.track_time)):
             if self.track_time[i] < time:
                 time_diff = time - self.track_time[i]
-                self.song.tracks[i].append(Message("note_on", note=0, channel=self.current_channel, velocity=0, time=0))
+                self.song.tracks[i].append(Message("note_on",  note=0, channel=self.current_channel, velocity=0, time=0))
                 self.song.tracks[i].append(Message("note_off", note=0, channel=self.current_channel, velocity=0, time=time_diff))
                 self.track_time[i] = time
 
 
     def midify_measure(self, signal):
+        print('measure')
         if signal['start']:
             self.current_track = 0
             self.current_channel = 0
-            self.first_instrument = True
             self.resolve_errors(self.measure_start_time)
         else:
             self.measure_start_time += self.ticks_per_beat * self.timesig[0]
+            self.resolve_errors(self.measure_start_time)
 
     def midify_timesig(self, signal):
+        print('timesig')
         if signal['type'] != 'timesig':
             print('Error: invalid timesig signal')
 
@@ -159,6 +156,7 @@ class MidiGenerator:
 
 
     def midify_dynamic(self, signal):
+        print('dynamic')
         if signal['type'] != 'dynamic':
             print('Error: invalid dynamic signal')
 
@@ -166,24 +164,17 @@ class MidiGenerator:
 
 
     def midify_instrument(self, signal):
+        print('instrument')
         instrumentNumber = instrumentToNumber[signal['name']]
 
-        if(self.first_instrument == True):
-            self.first_instrument = False
-        else:
-            self.current_channel += 1
-
-            if(self.current_track == self.max_track and self.first_instrument == False):
-                self.add_track()
-            elif(self.first_instrument == True):
-                self.first_instrument = False
-            else:
-                self.current_track += 1
+        self.current_track += 1
+        self.current_channel += 1
 
         return Message('program_change', channel=self.current_channel, program=instrumentNumber, time=0)
 
 
     def midify_tempo(self, signal):
+        print('tempo')
         if(signal['type'] != 'tempo'):
             print('Error when midifying tempo')
             return ''
@@ -193,6 +184,7 @@ class MidiGenerator:
 
 
     def midify_note(self, signal):
+        print('note')
         if(signal['type'] != 'note'):
             print('Error when midifying note')
             return '', ''
@@ -204,6 +196,7 @@ class MidiGenerator:
 
 
     def midify_rest(self, signal):
+        print('rest')
         if(signal['type'] != 'rest'):
             print('Error when midifying rest')
             return '', ''
@@ -211,13 +204,4 @@ class MidiGenerator:
         length = int(self.ticks_per_beat * self.timesig[0] * signal['length_num'] / signal['length_denom'])
         length = int(self.ticks_per_beat * self.timesig[1] * signal['length_num'] / signal['length_denom'])
         return Message('note_on', note=0, channel=self.current_channel, velocity=self.dynamic, time=0), Message('note_off',note=0, channel=self.current_channel, velocity=self.dynamic, time=length)
-
-
-    def add_track(self):
-        self.song.tracks.append(MidiTrack())
-        self.max_track += 1
-        self.current_track = self.max_track
-        self.track_time.append(self.measure_start_time)
-        self.song.tracks[self.max_track].append(Message("note_on", note=0, channel=self.current_channel, velocity=0, time=0))
-        self.song.tracks[self.max_track].append(Message("note_off", note=0, channel=self.current_channel, velocity=0, time=self.measure_start_time))
 
---
+++ src/signal.sig
@@ -0,0 +1,24 @@
+{'type': 'tempo', 'bpm': 120}
+{'type': 'timesig', 'time_num': 4, 'time_denom': 4}
+{'type': 'dynamic', 'volume': 'mf'}
+{'type': 'tempo', 'bpm': '100'}
+{'type': 'measure', 'start': True}
+{'type': 'instrument', 'name': 'acousticgrandpiano'}
+{'type': 'dynamic', 'volume': 'mp'}
+{'type': 'note', 'note_name': 'F2', 'length_num': 1, 'length_denom': 8}
+{'type': 'note', 'note_name': 'G2', 'length_num': 1, 'length_denom': 8}
+{'type': 'dynamic', 'volume': 'f'}
+{'type': 'chord', 'notes': ['C4', 'G4'], 'length_num': 1, 'length_denom': 4}
+{'type': 'chord', 'notes': ['D4', 'A5'], 'length_num': 1, 'length_denom': 4}
+{'type': 'chord', 'notes': ['E4', 'B5'], 'length_num': 1, 'length_denom': 4}
+{'type': 'measure', 'start': False}
+{'type': 'measure', 'start': True}
+{'type': 'instrument', 'name': 'acousticgrandpiano'}
+{'type': 'dynamic', 'volume': 'mp'}
+{'type': 'note', 'note_name': 'F2', 'length_num': 1, 'length_denom': 8}
+{'type': 'note', 'note_name': 'G2', 'length_num': 1, 'length_denom': 8}
+{'type': 'dynamic', 'volume': 'f'}
+{'type': 'chord', 'notes': ['C4', 'G4'], 'length_num': 1, 'length_denom': 4}
+{'type': 'chord', 'notes': ['D4', 'A5'], 'length_num': 1, 'length_denom': 4}
+{'type': 'chord', 'notes': ['E4', 'B5'], 'length_num': 1, 'length_denom': 4}
+{'type': 'measure', 'start': False}
